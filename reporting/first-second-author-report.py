9d23e4d61b75e32c58be63f78b3f0482152ca86b
houseplant <houseplant@users.noreply.github.com>
2015-11-19 14:23:38 -0700

    fixes type in comments

--- reporting/first-second-author-report.py
+++ reporting/first-second-author-report.py
@@ -1,27 +1,24 @@
-# the purpose of this script will be to identify osm metadata records that contain certain
-# instDivision affiliations for the first or second authors
-
-__author__ = 'jblomer'
 from lxml import etree
 import os
 
 
 class AuthorReporter():
+    '''
+    This class identifies osm metadata records that contain MMM affiliations for the first
+    or second authors. User inputs the directory path containing records to be parsed.
+    '''
 
     def __init__(self, directory):
         self.counter = 0
-        # self.file = file
         self.directory = directory
         for file in os.listdir(directory):
             if file.endswith(".xml"):
-                # print file
                 self.parseFile(os.path.join(directory, file))
 
 
     def parseFile(self, file):
         parser = etree.XMLParser(recover=True, encoding='utf-8')
         tree = etree.parse(file, parser)
-
         # print(etree.tostring(tree.getroot()))
 
         ns = {'osm': 'http://nldr.library.ucar.edu/metadata/osm'}
